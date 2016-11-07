0703955dde376b5ae5968dca25a57023395fc18c
leonwgc <guochao.wang@dianrong.com>
2016-11-07 17:32:56 +0800

    support long property binding

--- link.js
+++ link.js
@@ -52,6 +52,7 @@ function link(el, model) {
           propStack = propStack || [];
           propStack.push(prop);
           loadRender(model[prop], propStack)
+          propStack.pop();
         } else {
           if (propStack && propStack.length > 0) {
             var dotProp = propStack.slice(0);
@@ -126,124 +127,28 @@ function link(el, model) {
                   val = newVal;
 
                   if (propStack && propStack.length > 0) {
-                    prop = propStack.slice(0).join('.') + '.' + prop;
+                    propChangeHandler(propStack.join('.') + '.' + prop, newVal);
                   }
-                  propChangeHandler(prop, newVal);
+                  else {
+                    propChangeHandler(prop, newVal);
+                  }
 
                 }
               }
             })
-          })(prop, value);
+          })(prop, value, propStack && propStack.slice(0));
         }
       }
     }
   }
 
-  function modelBinder() {
-    var len = 0, item;
+  (function linkTogether() {
     scan(el);
     transformModel(model);
-
-    // len = ar.length;
-
-    // if (len > 0) {
-    //   for (var i = 0; i < len; i++) {
-    //     item = ar[i];
-
-    //     if (item.action === 'bind') {
-    //       item.el.innerText = model[item.prop];
-    //     }
-    //     else if (item.action === 'model') {
-    //       item.el.value = model[item.prop];
-    //     }
-    //   }
-    // }
-
-    loadRender(model);
-  }
-
-  modelBinder();
+    loadRender(model); // first time render model to view 
+  })();
 
 };
 
 link(document.getElementById('demo'), model)
 
-
-// function scanDom(el, ar, model) {
-//   if (el && ar && model) {
-//     if (el.hasAttribute && el.hasAttribute('lk-bind')) {
-//       ar.push({ el: el, prop: el.getAttribute('lk-bind'), action: 'bind' });
-//     }
-//     else if (el.hasAttribute && el.hasAttribute('lk-model')) {
-//       ar.push({ el: el, prop: el.getAttribute('lk-model'), action: 'model' });
-//       var prop = el.getAttribute('lk-model');
-//       el.addEventListener('keyup', function () {
-//         model[prop] = el.value || '';
-//       }, false);
-//     }
-//     var childNodes = el.childNodes;
-//     var len = childNodes.length;
-//     for (var i = 0; i < len; i++) {
-//       scanDom(childNodes[i], ar, model);
-//     }
-//   }
-// }
-
-// function transformModel(model, ar) {
-//   var len = ar.length;
-//   if (model) {
-//     for (var prop in model) {
-//       if (model.hasOwnProperty(prop)) {
-//         var value = model[prop];
-//         (function (prop, value) {
-//           var val = value, prop = prop;
-//           Object.defineProperty(model, prop, {
-//             get: function () {
-//               return val;
-//             },
-//             set: function (newVal) {
-//               val = newVal;
-//               var c = len, item;
-//               while (c--) {
-//                 item = ar[c];
-//                 if (item.prop === prop) {
-//                   if (item.action === 'bind') {
-//                     item.el.innerText = newVal;
-//                   }
-//                   else if (item.action === 'model') {
-//                     item.el.value = newVal;
-//                   }
-//                 }
-//               }
-//             }
-//           })
-//         })(prop, value);
-//       }
-//     }
-//   }
-// }
-
-// var modelBinder = function (el, model) {
-//   var ar = [], len = 0, item;
-//   scanDom(el, ar, model);
-
-//   transformModel(model, ar);
-
-//   len = ar.length;
-
-//   if (len > 0) {
-//     for (var i = 0; i < len; i++) {
-//       item = ar[i];
-
-//       if (item.action === 'bind') {
-//         item.el.innerText = model[item.prop];
-//       }
-//       else if (item.action === 'model') {
-//         item.el.value = model[item.prop];
-//       }
-//     }
-//   }
-// }
-
-
-// modelBinder(document.getElementById('demo'), model);
