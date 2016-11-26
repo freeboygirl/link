bfe69897a1c0b6acc41a3aee4605e104e948fc36
wangg <wangg@code.dianrong.com>
2016-11-26 19:47:05 +0800

    refactor ui render

--- src/parts/uiRender.js
+++ src/parts/uiRender.js
@@ -1,31 +1,41 @@
 function uiRenderFnBuilder(linkContext) {
   //return ui render fn
   return function () {
-    var exprVal;
-    if (linkContext.tpl) {
-      // interpolation
-      exprVal = evalInterpolation(linkContext);
-    }
-    else if (linkContext.expr) {
-      exprVal = evalExpr(linkContext);
-    }
-    else {
-      // just watch, no expr
-      exprVal = getWatchValue(linkContext.prop);
-    }
+    var exprVal = evalLinkValue(linkContext);
+    renderLink(linkContext, exprVal);
+  }
+}
 
-    if (linkContext.directive === 'x-bind') {
-      linkContext.el.textContent = exprVal;
-    }
-    else if (linkContext.directive === 'x-model') {
-      linkContext.el.value = exprVal;
-    } else if (linkContext.directive === 'x-show' || linkContext.directive === 'x-hide') {
-      showHideHanlder(linkContext, exprVal, linkContext.directive);
-    }
-    else if (linkContext.directive === 'x-repeat') {
-      // repeat can't be nested
-      // repeat item will construct a new linker object
-      repeatHanlder(linkContext);
-    }
+function evalLinkValue(linkContext) {
+  var exprVal;
+  if (linkContext.tpl) {
+    // interpolation
+    exprVal = evalInterpolation(linkContext);
+  }
+  else if (linkContext.expr) {
+    exprVal = evalExpr(linkContext);
+  }
+  else {
+    // just watch, no expr
+    exprVal = getWatchValue(linkContext.prop);
+  }
+
+  return exprVal;
+}
+
+
+function renderLink(linkContext, exprVal) {
+  if (linkContext.directive === 'x-bind') {
+    linkContext.el.textContent = exprVal;
+  }
+  else if (linkContext.directive === 'x-model') {
+    linkContext.el.value = exprVal;
+  } else if (linkContext.directive === 'x-show' || linkContext.directive === 'x-hide') {
+    showHideHanlder(linkContext, exprVal, linkContext.directive);
+  }
+  else if (linkContext.directive === 'x-repeat') {
+    // repeat can't be nested
+    // repeat item will construct a new linker object
+    repeatHanlder(linkContext);
   }
 }
