a23a55f02025bc28a89308cc06fe9c7c0ecbff4d
wangg <wangg@code.dianrong.com>
2016-11-29 20:33:42 +0800

    change demo js and editor conf

--- demo/demo.js
+++ demo/demo.js
@@ -1,35 +1,33 @@
-(function () {
-  window.model = {
-    name: 'leon', age: 2,
-    address: {
-      city: 'sh',
-      location:
-      { area: 'minhang', postcode: '110' }
-    },
-    loves: [{ name: 'mother', age: 50 }, { name: 'fater', age: 60 }],
-    other: {
-      hates: ['a', 'b', 'c']
-    },
-    trueAge: 18,
-    sex: 'male',
-    fruit: ['apple', 'banana'],
-    message: ''
-  };
+var model = {
+  name: 'leon', age: 2,
+  address: {
+    city: 'sh',
+    location:
+    { area: 'minhang', postcode: '110' }
+  },
+  loves: [{ name: 'mother', age: 50 }, { name: 'fater', age: 60 }],
+  other: {
+    hates: ['a', 'b', 'c']
+  },
+  trueAge: 18,
+  sex: 'male',
+  fruit: ['apple', 'banana'],
+  message: ''
+};
 
-  var methods = {
-    hello: function () {
-      console.log('hello ' + this.name, ' you are ' + this.age);
-    },
-    hi: function () {
-      console.log('hi ' + this.name);
-    },
-    focus: function (ev, el) {
-      el.style.background = 'yellow';
-    },
-    blur: function (ev, el) {
-      el.style.background = '';
-    }
-  };
+var methods = {
+  hello: function () {
+    console.log('hello ' + this.name, ' you are ' + this.age);
+  },
+  hi: function () {
+    console.log('hi ' + this.name);
+  },
+  focus: function (ev, el) {
+    el.style.background = 'yellow';
+  },
+  blur: function (ev, el) {
+    el.style.background = '';
+  }
+};
 
-  linker = link(document.getElementById('demo'), model, methods);
-})();
+var linker = link(document.getElementById('demo'), model, methods);
