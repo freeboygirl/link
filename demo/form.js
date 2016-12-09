210e4ec481a827420b0913b830f88cd34e8082e5
leonwgc <guochao.wang@dianrong.com>
2016-12-09 17:44:37 +0800

    form with checbox

--- demo/form.js
+++ demo/form.js
@@ -2,15 +2,17 @@ var model = {
   email: '',
   password: '',
   remember: [],
-  greeting: ''
+  greeting: '',
+  fruit: []
 };
 
 var methods = {
   remeberClick: function () {
     this.greeting = 'you chooosed  ' + (this.remember.at(0) === 'yes' ? 'remember' : 'forget it');
   },
-  signin: function () {
-    this.greeting = 'hello ' + this.email + ', your password is' + this.password + ', and you chooosed  ' + (this.remember.at(0) === 'yes' ? 'remember' : 'forget it');
+  clickme: function () {
+    this.greeting = 'hello ' + this.email + ', your password is' + this.password + ', and you chooosed  ' + (this.remember.at(0) === 'yes' ? 'remember' : 'forget it') +
+      'your fav fruit is ' + this.fruit.getArray().join(',');
   }
 };
 
