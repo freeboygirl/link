e16ee5bf1318d80078686ef4f93010280d8b2581
leonwgc <guochao.wang@dianrong.com>
2016-12-23 13:15:43 +0800

    prevent method name override the model

--- src/parts/behaviors.js
+++ src/parts/behaviors.js
@@ -4,6 +4,9 @@ Link.prototype.addBehaviors = function addBehaviors() {
     var methods = Object.keys(this.behaviors);
     each(methods, function (fn) {
       if (isFunction(that.behaviors[fn])) {
+        if (fn in that.model) {
+          throw linkError('{0} is defined in the data model,please change the function/method name of "{0}"', fn)
+        }
         that.model[fn] = that.behaviors[fn];
       }
     });
