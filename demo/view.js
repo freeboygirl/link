90d9d8b3a844ae954c867ae81192630a2510f5ca
wangg <wangg@code.dianrong.com>
2016-12-25 16:06:26 +0800

    add router demo

---
+++ demo/view.js
@@ -0,0 +1,36 @@
+var model = {
+  test: 'hi, welcome to x-view example',
+};
+
+var methods = {
+
+};
+
+var timerId = 'demo';
+console.time(timerId);
+
+var linker = link(model);
+
+link.route(linker, [
+  {
+    path: '/home',
+    model: {
+      name: 'wgc',
+      age: 18
+    },
+    actions: {
+      clickme: function () {
+        console.log(this);
+        alert('hello ' + this.name + ', you are ' + this.age);
+      }
+    },
+    templateUrl: 'views/tpl1.html'
+  },
+  {
+    path: '/index',
+    templateUrl: 'views/tpl2.html'
+  }
+], '/index');
+
+
+console.timeEnd(timerId)
---
+++ src/directives/view.js
@@ -0,0 +1,3 @@
+function viewHanlder(linker) {
+  linker.route();
+}
