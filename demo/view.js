f05e1cab165eea8172a92c34950f4258f15306c7
leonwgc <guochao.wang@dianrong.com>
2016-12-26 16:52:07 +0800

    update view demo

--- demo/view.js
+++ demo/view.js
@@ -9,38 +9,41 @@ var methods = {
 var timerId = 'demo';
 console.time(timerId);
 
-var linker = link(model);
-
-link.route(linker, [
-  {
-    path: '/home',
-    model: {
-      name: 'wgc',
-      age: 18
+var routeConfig = {
+  defaultPath: '/index',
+  routes: [
+    {
+      path: '/home',
+      model: {
+        name: 'wgc',
+        age: 18
+      },
+      actions: {
+        clickme: function () {
+          console.log(this);
+          alert('hello ' + this.name + ', you are ' + this.age);
+        }
+      },
+      preLink: function () {
+        this.model.name = 'leon',
+          this.model.gender = 'male';
+      },
+      templateUrl: 'views/tpl1.html'
     },
-    actions: {
-      clickme: function () {
-        console.log(this);
-        alert('hello ' + this.name + ', you are ' + this.age);
+    {
+      path: '/index',
+      templateUrl: 'views/tpl2.html'
+    },
+    {
+      path: '/pre',
+      preLink: function () {
+        location.href = 'index.html';
       }
-    },
-    preLink:function(){
-      this.model.name='leon',
-      this.model.gender='male';
-    },
-    templateUrl: 'views/tpl1.html'
-  },
-  {
-    path: '/index',
-    templateUrl: 'views/tpl2.html'
-  },
-   {
-    path: '/pre',
-    preLink:function(){
-      location.href='index.html';
     }
-  }
-], '/index');
+  ]
+}
+
+var linker = link(model,null,routeConfig);
 
 
 console.timeEnd(timerId)
