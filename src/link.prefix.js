af7540e20a179d2599dcffbe52a9083ca2dd64b6
leonwgc <guochao.wang@dianrong.com>
2016-12-27 11:30:46 +0800

    simplify link factory

--- src/link.prefix.js
+++ src/link.prefix.js
@@ -1,33 +1,8 @@
 'use strict';
 (function () {
-  window.link = function linkFactory(el, data, behaviors, routeConfig) {
-    var _el = el, _data = data, _behaviors = behaviors, _routeConfig = routeConfig;
-    if (arguments.length === 0 || arguments.length === 1 && isObject(el) && ('nodeName' in el)) {
-      throw linkError('data must be specified for a link');
-    }
-    if (arguments.length === 1 && !isObject(el)) {
-      throw linkError('data must be an object');
-    }
-    if (!('nodeName' in el)) {
-      _el = document;
-      _data = el;
-      _behaviors = data;
-      _routeConfig = behaviors;
-    }
-    if (!isObject(_data)) {
-      throw linkError('data must be an object');
-    }
-    if (_behaviors && !isObject(_behaviors)) {
-      throw linkError('behaviors must be an object with function props');
-    }
-
-    if (_routeConfig) {
-      if (!(isArray(_routeConfig.routes) && _routeConfig.defaultPath)) {
-        throw linkError('routeConfig must contain route array and defaultPath.');
-      }
-    }
-
-    return new Link(_el, _data, _behaviors, _routeConfig);
+  window.link = function linkFactory(config) {
+    config = extend({}, { el: window.document, model: {}, methods: null, routes: null }, config);
+    return new Link(config.el, config.model, config.methods, config.routes);
   };
   link.helper = {
     isObject: isObject,
