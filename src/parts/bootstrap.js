1a98a62a0843c0be13f49d7b5866fe0d952d19e8
leonwgc <guochao.wang@dianrong.com>
2016-12-29 11:24:47 +0800

    refa $$watched place

--- src/parts/bootstrap.js
+++ src/parts/bootstrap.js
@@ -2,8 +2,8 @@ Link.prototype.bootstrap = function () {
   if (this.model.hasOwnProperty('$$watched')) {
     throw linkError('this model had been used for some linker, please check...');
   }
-  this.watchModel(this.model, []);
   _def_const_prop_(this.model, '$$watched', true);
+  this.watchModel(this.model, []);
   this.compile(this.el);
   this.render();
   this.addBehaviors();
