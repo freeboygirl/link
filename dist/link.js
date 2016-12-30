b610ed6b3aebc836e0223bd3564962e82221938d
leonwgc <wangg@code.dianrong.com>
2016-12-30 20:31:02 +0800

    update lib

--- dist/link.js
+++ dist/link.js
@@ -94,7 +94,7 @@ function arrayRemove(arr, value) {
   var len = arr.length;
   for (var i = 0; i < len; i++) {
     if (arr[i] === value) {
-      arr.splice(i, 1);
+      arr.splice(i--, 1);
       len--;
     }
   }
